41359eacc83f8f953aa66e642e93136a77809748
Aslah Fuadin <fuadinaslah@gmail.com>
2024-07-29 20:50:04 +0300

    udpate CustomerOrderRepository

--- src/Repositories/CustomerOrderRepository.cs
+++ src/Repositories/CustomerOrderRepository.cs
@@ -2,28 +2,41 @@ using System.Linq.Expressions;
 using KanzwayCron.Data.Context;
 using KanzwayCron.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.SqlClient;
 
-namespace KanzwayCron.Repositories;
-
-public class CustomerOrderRepository(KanzDbContext kanzDb) : ICustomerOrderRepository
+namespace KanzwayCron.Repositories
 {
-    private readonly KanzDbContext _kanzDb = kanzDb;
-
-    public IEnumerable<CustomerOrder> GetCustomerOrders(Expression<Func<CustomerOrder, bool>> predicate)
+    public class CustomerOrderRepository : ICustomerOrderRepository
     {
-        var result = _kanzDb.CustomerOrders.Where(predicate).ToList();
-        return result;
-    }
+        private readonly KanzDbContext _kanzDb;
 
-    public void UpdateCustomerOrders(IEnumerable<CustomerOrder> customerOrders)
-    {
-        if (customerOrders == null || !customerOrders.Any()) return;
-        
-        foreach (var customerOrder in customerOrders)
+        public CustomerOrderRepository(KanzDbContext kanzDb)
         {
-            _kanzDb.Entry(customerOrder).CurrentValues.SetValues(customerOrder);
+            _kanzDb = kanzDb;
         }
 
-        _kanzDb.SaveChanges();
+        public IEnumerable<CustomerOrder> GetCustomerOrders(Expression<Func<CustomerOrder, bool>> predicate)
+        {
+            return _kanzDb.CustomerOrders.Where(predicate).ToList();
+        }
+
+        public void UpdateCustomerOrders(IEnumerable<CustomerOrder> customerOrders)
+        {
+            if (customerOrders == null || !customerOrders.Any()) return;
+
+            foreach (var customerOrder in customerOrders)
+            {
+                var sql = "UPDATE [Transaction].[CustomerOrder] " +
+                          "SET [Status] = @status, [UpdatedAt] = @updatedAt " +
+                          "WHERE [Id] = @id";
+
+                _kanzDb.Database.ExecuteSqlRaw(sql,
+                    new SqlParameter("@status", customerOrder.Status),
+                    new SqlParameter("@updatedAt", customerOrder.UpdatedAt),
+                    new SqlParameter("@id", customerOrder.Id));
+            }
+        }
     }
 }
