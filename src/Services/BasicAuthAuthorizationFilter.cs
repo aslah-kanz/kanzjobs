d4871c4d4df7feada9c08f1a71c66e65fc212adb
Aslah Fuadin <fuadinaslah@gmail.com>
2024-07-29 15:03:35 +0300

    fix: auth

--- src/Services/BasicAuthAuthorizationFilter.cs
+++ src/Services/BasicAuthAuthorizationFilter.cs
@@ -1,21 +1,45 @@
-using Hangfire.Annotations;
 using Hangfire.Dashboard;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
 
 public class BasicAuthAuthorizationFilter : IDashboardAuthorizationFilter
 {
-    private readonly string _authenticationScheme;
+    private readonly string _username;
+    private readonly string _password;
 
-    public BasicAuthAuthorizationFilter(string authenticationScheme)
+    public BasicAuthAuthorizationFilter(string username, string password)
     {
-        _authenticationScheme = authenticationScheme;
+        _username = username;
+        _password = password;
     }
 
-    public bool Authorize([NotNull] DashboardContext context)
+    public bool Authorize(DashboardContext context)
     {
         var httpContext = context.GetHttpContext();
-        return Task.Run(() => httpContext.AuthenticateAsync(_authenticationScheme)).Result.Succeeded;
+
+        if (httpContext.User.Identity.IsAuthenticated)
+        {
+            return true;
+        }
+
+        var authHeader = httpContext.Request.Headers["Authorization"];
+
+        if (authHeader.Count > 0)
+        {
+            var authHeaderValue = AuthenticationHeaderValue.Parse(authHeader);
+            var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeaderValue.Parameter)).Split(':');
+            var username = credentials[0];
+            var password = credentials[1];
+
+            if (username == _username && password == _password)
+            {
+                var claims = new[] { new Claim("name", username), new Claim(ClaimTypes.Role, "Administrator") };
+                var identity = new ClaimsIdentity(claims, "Basic");
+                httpContext.User = new ClaimsPrincipal(new ClaimsIdentity(identity));
+                return true;
+            }
+        }
+
+        httpContext.Response.Headers["WWW-Authenticate"] = "Basic";
+        httpContext.Response.StatusCode = 401;
+        return false;
     }
 }
